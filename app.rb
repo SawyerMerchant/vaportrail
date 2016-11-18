1f678a5766e27f82d8d8c1fe5325d4d4fe9e76f5
John Sawyer <sawyer@sawyermerchant.com>
2016-11-18 14:01:33 -0500

    Cookies

--- app.rb
+++ app.rb
@@ -6,10 +6,10 @@ require 'sinatra/reloader'
 
 
 get "/" do
-  handle = "@sawyermerchant" #request.cookies["handle"] || "@example"
+  handle = request.cookies["handle"] || "@example"
   erb :add, locals:{handle: handle}
 end
 
 post "/add" do
-  # response.set_cookie("handle", params[:handle])
+  response.set_cookie("handle", params[:handle])
 end
